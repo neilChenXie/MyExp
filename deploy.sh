edbd52a150881a471580f6c22615d84a8b788f21
Chen Xie <xiec@usc.edu>
2016-08-15 11:05:21 +0800

    log_category

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-jekyll build
+if [ -z $1 ];
+then
+	echo "1st arg required"
 
-scp -r _site/* root@172.16.1.114:/opt/apache/htdocs/Sekorm/
+else
+	git add -A .
+	git commit -m "$1"
+	git push 
+
+	jekyll build
+
+	scp -r _site/* root@172.16.1.114:/opt/apache/htdocs/Sekorm/
+fi
